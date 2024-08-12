d5ff4003cd4cd5080c962a7438329e44fe75f061
Envi-Scripts <126303539+Envi-Scripts@users.noreply.github.com>
2024-08-12 23:49:35 +0100

    Add files via upload

---
+++ envi-shells-starter-pack/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game { 'gta5' } 
+
+author 'Envi-Scripts'
+description 'Addon Shells By Envi-Scripts'
+version '2.0.0'
+
+
+
+-- Empty shell interiors
+data_file 'DLC_ITYP_REQUEST' 'stream/envi_shell_02_empty.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/envi_shell_01_empty.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/envi_shell_03_empty.ytyp'
+
+-- Furnished shell interiors
+data_file 'DLC_ITYP_REQUEST' 'stream/envi_shell_01_furnished.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/envi_shell_02_furnished.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/envi_shell_03_furnished.ytyp'
+
+-- Note: This resource provides 3 different shell types, each with an empty and furnished variant
